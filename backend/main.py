b61571449bb07ff7ad0c3bd0b0ae4f940b5487d3
candatagagan candata <gagan@candata.ai>
2023-09-26 04:00:21 +0000

    Initial Commit

---
+++ backend/main.py
@@ -0,0 +1,5 @@
+from utilities.resume_ai import Sentence_Similarity
+
+sen = Sentence_Similarity()
+
+sen.calculate_embedding()
---
+++ backend/utilities/resume_ai.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from numpy.linalg import norm
+
+class Sentence_Similarity:
+
+    def __init__(self):
+        pass
+
+    def cosine_similarity(input_sentences, calculate_sentence) -> int:
+        cosine = np.dot(input_sentences,calculate_sentence)/(norm(input_sentences)*norm(calculate_sentence))
+        return cosine
+
+    def calculate_embedding(sentences: list, model_name: str) -> list:
+        model = SentenceTransformer(model_name)
+        return model.encode(sentence)
