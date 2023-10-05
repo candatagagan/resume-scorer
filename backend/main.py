263094810dde913b6382a4adf48f0a26c53fba5c
Gagan Nayyar <gagan.nayyar@outlook.com>
2023-10-05 13:48:35 +0000

    One sentence scoring part

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,23 @@
 from utilities.resume_ai import Sentence_Similarity
+import json
+
+variables_file = open('/workspace/resume-scorer/backend/utilities/variables.json')
+
+variables = json.load(variables_file)
 
 sen = Sentence_Similarity()
 
-sen.calculate_embedding()
+input_sentence = "I am skilled in data engineering"
+
+compare_sentences = ["I am skilled in data science", "I am skilled in data engineering", "I am skilled in data"]
+
+
+if __name__ == "__main__":
+    input_embeddings = sen.calculate_embedding([input_sentence], variables["model"])
+    output_embeddings = [sen.calculate_embedding([i], variables["model"]) for i in compare_sentences]
+    output_embeddings_dict = dict(zip(compare_sentences, output_embeddings))
+    #print(output_embeddings_dict)
+
+    score = [sen.cosine_similarity(input_embeddings, i) for i in output_embeddings_dict.values()]
+    score_dict = dict(zip(compare_sentences, score))
+    print(score_dict)    
--- backend/utilities/resume_ai.py
+++ backend/utilities/resume_ai.py
@@ -7,10 +7,10 @@ class Sentence_Similarity:
     def __init__(self):
         pass
 
-    def cosine_similarity(input_sentences, calculate_sentence) -> int:
-        cosine = np.dot(input_sentences,calculate_sentence)/(norm(input_sentences)*norm(calculate_sentence))
-        return cosine
+    def cosine_similarity(self, input_sentences, calculate_sentence) -> int:
+        cosine = np.dot(input_sentences,calculate_sentence.T)/(norm(input_sentences)*norm(calculate_sentence))
+        return float(cosine[0])
 
-    def calculate_embedding(sentences: list, model_name: str) -> list:
+    def calculate_embedding(self, sentences: list, model_name: str) -> list:
         model = SentenceTransformer(model_name)
-        return model.encode(sentence)
+        return model.encode(sentences)
