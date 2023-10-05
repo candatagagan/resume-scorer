3129e1dc10d3942393558e0cb7c09002e88410e4
Gagan Nayyar <gagan.nayyar@outlook.com>
2023-10-05 14:04:27 +0000

    Need to create a fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from utilities.resume_ai import Sentence_Similarity
 import json
 
+
 variables_file = open('/workspace/resume-scorer/backend/utilities/variables.json')
 
 variables = json.load(variables_file)
@@ -11,13 +12,17 @@ input_sentence = "I am skilled in data engineering"
 
 compare_sentences = ["I am skilled in data science", "I am skilled in data engineering", "I am skilled in data"]
 
-
-if __name__ == "__main__":
-    input_embeddings = sen.calculate_embedding([input_sentence], variables["model"])
-    output_embeddings = [sen.calculate_embedding([i], variables["model"]) for i in compare_sentences]
+def score_calculator(input_sentence, compare_sentences, model) -> dict:
+    input_embeddings = sen.calculate_embedding([input_sentence], model)
+    output_embeddings = [sen.calculate_embedding([i], model) for i in compare_sentences]
     output_embeddings_dict = dict(zip(compare_sentences, output_embeddings))
-    #print(output_embeddings_dict)
-
     score = [sen.cosine_similarity(input_embeddings, i) for i in output_embeddings_dict.values()]
     score_dict = dict(zip(compare_sentences, score))
-    print(score_dict)    
+    return score_dict  
+
+if __name__ == "__main__":
+    score_calculator(
+                    input_sentence=input_sentence,
+                    compare_sentences=compare_sentences, 
+                    model=variables["model"]
+                    )
